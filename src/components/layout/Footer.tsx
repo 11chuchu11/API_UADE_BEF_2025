f69e2c4761df2d2ca0232a8f07ceef579eaf8665
Franco Veron Peralta <francoveronperalta@gmail.com>
2025-09-22 19:03:55 -0300

    HomeTexts: move folder from components to home

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,63 +5,59 @@ import { FiInstagram, FiFacebook, FiYoutube } from 'react-icons/fi'
 import { Button } from '@components/ui/button'
 
 // Texts
-import { getTexts } from '../text'
+import { getTexts } from '../Home/text'
 
 const Footer: React.FC = () => {
+	const texts = getTexts()
+	const currentYear = new Date().getFullYear()
 
-  const texts = getTexts();
-  const currentYear = new Date().getFullYear()
+	return (
+		<footer className="bg-primary text-background px-20 py-8 md:py-10">
+			<div className="max-w-[95rem] mx-auto">
+				<div className="flex flex-col md:flex-row items-start md:items-center gap-6 md:gap-0">
+					<div className="space-y-1">
+						<h2 className="font-bold text-xl">{texts.footer.text_name_doctor}</h2>
+						<p className="text-sm opacity-90">{texts.footer.text_dev_signature(currentYear)}</p>
+						<p className="text-sm opacity-90">{texts.footer.copyright}</p>
+					</div>
 
-  return (
-    <footer className="bg-primary text-background px-20 py-8 md:py-10">
-      <div className="max-w-[95rem] mx-auto">
-        <div className="flex flex-col md:flex-row items-start md:items-center gap-6 md:gap-0">
-          <div className="space-y-1">
-            <h2 className="font-bold text-xl">{texts.footer.text_name_doctor}</h2>
-            <p className="text-sm opacity-90">{texts.footer.text_dev_signature(currentYear)}</p>
-            <p className="text-sm opacity-90">{texts.footer.copyright}</p>
-          </div>
+					<div className="flex-1" />
 
-          <div className="flex-1" />
+					<div className="flex gap-4">
+						<Button
+							asChild
+							className="rounded-full px-0 w-10 h-10 border border-background text-background bg-transparent transition-all duration-200 hover:bg-secondary hover:text-primary hover:border-primary transform hover:-translate-y-0.5"
+							variant="ghost"
+							size="icon">
+							<a href="https://instagram.com" aria-label="Instagram" rel="noopener noreferrer" target="_blank">
+								<FiInstagram size={18} />
+							</a>
+						</Button>
 
-          <div className="flex gap-4">
-            <Button
-              asChild
-              className="rounded-full px-0 w-10 h-10 border border-background text-background bg-transparent transition-all duration-200 hover:bg-secondary hover:text-primary hover:border-primary transform hover:-translate-y-0.5"
-              variant="ghost"
-              size="icon"
-            >
-              <a href="https://instagram.com" aria-label="Instagram" rel="noopener noreferrer" target="_blank">
-                <FiInstagram size={18} />
-              </a>
-            </Button>
+						<Button
+							asChild
+							className="rounded-full px-0 w-10 h-10 border border-background text-background bg-transparent transition-all duration-200 hover:bg-secondary hover:text-primary hover:border-primary transform hover:-translate-y-0.5"
+							variant="ghost"
+							size="icon">
+							<a href="https://facebook.com" aria-label="Facebook" rel="noopener noreferrer" target="_blank">
+								<FiFacebook size={18} />
+							</a>
+						</Button>
 
-            <Button
-              asChild
-              className="rounded-full px-0 w-10 h-10 border border-background text-background bg-transparent transition-all duration-200 hover:bg-secondary hover:text-primary hover:border-primary transform hover:-translate-y-0.5"
-              variant="ghost"
-              size="icon"
-            >
-              <a href="https://facebook.com" aria-label="Facebook" rel="noopener noreferrer" target="_blank">
-                <FiFacebook size={18} />
-              </a>
-            </Button>
-
-            <Button
-              asChild
-              className="rounded-full px-0 w-10 h-10 border border-background text-backgroundbg-transparent transition-all duration-200 hover:bg-secondary hover:text-primary hover:border-primary transform hover:-translate-y-0.5"
-              variant="ghost"
-              size="icon"
-            >
-              <a href="https://youtube.com" aria-label="YouTube" rel="noopener noreferrer" target="_blank">
-                <FiYoutube size={18} />
-              </a>
-            </Button>
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+						<Button
+							asChild
+							className="rounded-full px-0 w-10 h-10 border border-background text-backgroundbg-transparent transition-all duration-200 hover:bg-secondary hover:text-primary hover:border-primary transform hover:-translate-y-0.5"
+							variant="ghost"
+							size="icon">
+							<a href="https://youtube.com" aria-label="YouTube" rel="noopener noreferrer" target="_blank">
+								<FiYoutube size={18} />
+							</a>
+						</Button>
+					</div>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export { Footer }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,110 +1,106 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 // Components
-import { Button } from '@components/ui/button';
+import { Button } from '@components/ui/button'
 
 // Utils
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils'
 
 // texts
-import { getTexts } from '../text';
+import { getTexts } from '../Home/text'
 import { Link } from 'react-router';
 const Header: React.FC = () => {
+	const texts = getTexts()
 
-    const texts = getTexts();
+	const [scrolled, setScrolled] = useState(false)
 
-    const [scrolled, setScrolled] = useState(false);
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrolled(window.scrollY > 0)
+		}
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrolled(window.scrollY > 0);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+	return (
+		<header
+			className={cn(
+				'w-full fixed z-50 transition-all duration-1000 ease-in-out',
+				scrolled
+					? 'bg-primary text-white px-20 py-4 shadow-lg' // brand-green
+					: 'bg-secondary text-white px-20 py-4' // brand-green-light
+			)}>
+			<div className="flex items-center justify-between">
+				{/* Logo/Título */}
+				<div>
+					<h1
+						className={cn(
+							'text-xl font-bold transition-colors duration-1000 ease-in-out',
+							scrolled ? 'text-background' : 'text-primary' // brand-beige : brand-green
+						)}>
+						{texts.header.h1}
+					</h1>
+				</div>
 
-    return (
-        <header className={cn(
-            "w-full fixed z-50 transition-all duration-1000 ease-in-out",
-            scrolled
-                ? "bg-primary text-white px-20 py-4 shadow-lg" // brand-green
-                : "bg-secondary text-white px-20 py-4" // brand-green-light
-        )}>
-            <div className="flex items-center justify-between">
-                {/* Logo/Título */}
-                <div>
-                    <h1 className={cn(
-                        "text-xl font-bold transition-colors duration-1000 ease-in-out",
-                        scrolled ? "text-background" : "text-primary" // brand-beige : brand-green
-                    )}>
-                        {texts.header.h1}
-                    </h1>
-                </div>
+				{/* Navegación */}
+				<nav className="flex gap-24">
+					<a
+						href="#home"
+						className={cn(
+							'font-sans transition-all duration-500 ease-in-out',
+							scrolled
+								? 'text-background hover:text-muted' // brand-beige hover: brand-green-light-02
+								: 'text-primary hover:text-chart-2' // brand-green hover: brand-green-hover
+						)}>
+						{texts.header.navbar.link_1}
+					</a>
+					<a
+						href="#home"
+						className={cn(
+							'font-sans transition-all duration-500 ease-in-out',
+							scrolled
+								? 'text-background hover:text-muted' // brand-beige hover: brand-green-light-02
+								: 'text-primary hover:text-chart-2' // brand-green hover: brand-green-hover
+						)}>
+						{texts.header.navbar.link_2}
+					</a>
+					<a
+						href="#home"
+						className={cn(
+							'font-sans transition-all duration-500 ease-in-out',
+							scrolled
+								? 'text-background hover:text-muted' // brand-beige hover: brand-green-light-02
+								: 'text-primary hover:text-chart-2' // brand-green hover: brand-green-hover
+						)}>
+						{texts.header.navbar.link_3}
+					</a>
+					<a
+						href="#home"
+						className={cn(
+							'font-sans transition-all duration-500 ease-in-out',
+							scrolled
+								? 'text-background hover:text-mute' // brand-beige hover: brand-green-light-02
+								: 'text-primary hover:text-chart-2' // brand-green hover: brand-green-hover
+						)}>
+						{texts.header.navbar.link_4}
+					</a>
+				</nav>
 
-                {/* Navegación */}
-                <nav className="flex gap-24">
-                    <a
-                        href="#home"
-                        className={cn(
-                            "font-sans transition-all duration-500 ease-in-out",
-                            scrolled
-                                ? "text-background hover:text-muted" // brand-beige hover: brand-green-light-02
-                                : "text-primary hover:text-chart-2" // brand-green hover: brand-green-hover
-                        )}
-                    >
-                        {texts.header.navbar.link_1}
-                    </a>
-                    <a
-                        href="#home"
-                        className={cn(
-                            "font-sans transition-all duration-500 ease-in-out",
-                            scrolled
-                                ? "text-background hover:text-muted" // brand-beige hover: brand-green-light-02
-                                : "text-primary hover:text-chart-2" // brand-green hover: brand-green-hover
-                        )}
-                    >
-                        {texts.header.navbar.link_2}
-                    </a>
-                    <a
-                        href="#home"
-                        className={cn(
-                            "font-sans transition-all duration-500 ease-in-out",
-                            scrolled
-                                ? "text-background hover:text-muted" // brand-beige hover: brand-green-light-02
-                                : "text-primary hover:text-chart-2" // brand-green hover: brand-green-hover
-                        )}
-                    >
-                        {texts.header.navbar.link_3}
-                    </a>
-                    <a
-                        href="#home"
-                        className={cn(
-                            "font-sans transition-all duration-500 ease-in-out",
-                            scrolled
-                                ? "text-background hover:text-mute" // brand-beige hover: brand-green-light-02
-                                : "text-primary hover:text-chart-2" // brand-green hover: brand-green-hover
-                        )}
-                    >
-                        {texts.header.navbar.link_4}
-                    </a>
-                </nav>
-
-                {/* Botón Login */}
-                <Button asChild
-                    className={cn(
-                        "rounded-full uppercase px-4 py-2 transition-all duration-500 ease-in-out border",
-                        scrolled
-                            ? "bg-primary text-primary-foreground border-primary-foreground hover:bg-primary-foreground hover:text-primary hover:border-primary"
-                            : "bg-primary-foreground text-primary border-primary hover:bg-primary hover:text-primary-foreground hover:border-primary-foreground"
-                    )}
-                >
+				{/* Botón Login */}
+				<Button asChild
+					className={cn(
+						'rounded-full uppercase px-4 py-2 transition-all duration-500 ease-in-out border',
+						scrolled
+							? 'bg-primary text-primary-foreground border-primary-foreground hover:bg-primary-foreground hover:text-primary hover:border-primary'
+							: 'bg-primary-foreground text-primary border-primary hover:bg-primary hover:text-primary-foreground hover:border-primary-foreground'
+					)}>
                     <Link to="/admin">
-                        {texts.header.text_login}
+    					{texts.header.text_login}
                     </Link>
-                </Button>
-            </div>
-        </header>
-    );
-};
+				</Button>
+			</div>
+		</header>
+	)
+}
 
-export { Header }; 
+export { Header }
