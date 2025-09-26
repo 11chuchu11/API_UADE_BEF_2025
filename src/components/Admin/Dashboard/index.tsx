6394e45cc5cfa3ef6e3d7a59b3b66de3558e9601
bcapriz <brucapriz@icloud.com>
2025-09-25 22:39:59 -0300

    feature: We change botton login, and create hamburger menu for mobile version

---
+++ src/components/Admin/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+export default function AdminDashboard() {
+    return (
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 py-12">
+        <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+      </div>
+    );
+  }
+  
--- src/components/Admin/text/index.ts
+++ src/components/Admin/text/index.ts
@@ -2,6 +2,7 @@ export const getTextsAdmin = () => ({
     header: {
         h1: "Dr Daniel Diaz",
         text_center:"Agenda de Citas",
-        text_logout: "cerrar sesión"
+        text_logout: "cerrar sesión",
+        btn_settings: "Configuración"
     },
 })
